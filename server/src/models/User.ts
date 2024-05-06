3fa2fdb2982f5695ef4b9975c1ec9737c97987f0
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-06 22:41:53 +0300

    Create user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface UserDocument extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre<UserDocument>("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
