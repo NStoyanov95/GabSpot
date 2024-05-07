b55de37ea092252e58e5a579776bbb05c10ee856
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-07 09:58:42 +0300

    Implement login functionality

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { UserData } from "../types/User";
 
-import { register } from "../services/userService";
+import { register, login } from "../services/userService";
 
 const router = express.Router();
 
@@ -10,6 +10,22 @@ router.post("/register", async (req: Request, res: Response) => {
 
   try {
     const { _id, email, username, accessToken } = await register(userData);
+    res.cookie("auth-cookie", accessToken, { httpOnly: true, secure: true });
+    res.send({ email, username, _id });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(403).send({ error: error.message });
+    } else {
+      res.status(500).send({ error: "An unknown error occurred" });
+    }
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const userData: UserData = req.body;
+
+  try {
+    const { _id, email, username, accessToken } = await login(userData);
     res.cookie("auth-cookie", accessToken, { httpOnly: true, secure: true });
     res.send({ email, username, _id });
   } catch (error: unknown) {
