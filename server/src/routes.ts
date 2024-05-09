3e143f7d1c2074cf50c3fcc45aff6cdd840a69c1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-09 22:17:12 +0300

    Add post controller to router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import userController from "./controllers/userController";
+import postController from "./controllers/postController";
 
 const router = express.Router();
 
 router.use("/users", userController);
+router.use("/posts", postController);
 
 export default router;
