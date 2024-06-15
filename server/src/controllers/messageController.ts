ce8e6f45df669e04cb5ad0a330baac760e5cf4f3
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-15 22:56:40 +0300

    Create path for get single user messages

--- server/src/controllers/messageController.ts
+++ server/src/controllers/messageController.ts
@@ -1,17 +1,26 @@
 import express, { Request, Response } from "express";
 import messageService from "../services/messageService";
-import { MessageData } from "../types/Message";
+import { MessageData, MessageType } from "../types/Message";
 
 const router = express.Router();
 
 router.post("/send", async (req: Request, res: Response) => {
     const messageData: MessageData = req.body;
     try {
-        const message = await messageService.create(messageData);
+        const message: MessageType = await messageService.create(messageData);
         res.status(200).json(message);
     } catch (error) {
         res.status(500).json({ error: "Internal server error" });
     }
+});
+
+router.get("/:userId", async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    try {
+        const messages: MessageType[] | null =
+            await messageService.getMessagesForUser(userId);
+        res.status(200).json(messages);
+    } catch (error) {}
 });
 
 export default router;
