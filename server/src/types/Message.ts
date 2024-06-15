82520a411a195e50c2a669b58ef0df1438031976
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-15 22:08:53 +0300

    Update message interface

--- server/src/types/Message.ts
+++ server/src/types/Message.ts
@@ -1,8 +1,15 @@
 import { Types } from "mongoose";
 
 export interface MessageType {
+    _id: Types.ObjectId;
     from: Types.ObjectId;
     to: Types.ObjectId;
     content: string;
     createdAt?: Date;
+}
+
+export interface MessageData {
+    from: Types.ObjectId;
+    to: Types.ObjectId;
+    content: string;
 }
