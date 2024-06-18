05fb9b4d7018ad21c08a90ca6a306c3f0ab4d3bd
NStoyanov95 <nstoyanov9512@gmail.com>
2024-06-18 21:24:50 +0300

    Add followers and following to user types

--- server/src/types/User.ts
+++ server/src/types/User.ts
@@ -1,10 +1,12 @@
-import { Types } from "mongoose";
+import { Schema, Types } from "mongoose";
 
 export interface UserType {
     _id?: Types.ObjectId;
     username: string;
     email: string;
     password: string;
+    followers: Types.ObjectId[];
+    following: Types.ObjectId[];
 }
 
 export interface UserData {
