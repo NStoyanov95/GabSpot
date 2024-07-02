d443ace81aa24c715e4efc914dc411ea977e9dc5
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-02 07:20:02 +0300

    Create card section component

---
+++ client/gabSpot/src/components/CardSection/CardSection.jsx
@@ -0,0 +1,9 @@
+import "./CardSection.css";
+function CardSection() {
+    return (
+        <div className="card-section">
+            <div className="card-container"></div>
+        </div>
+    );
+}
+export default CardSection;
--- client/gabSpot/src/components/Dashboard/Dashboard.jsx
+++ client/gabSpot/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
+import CardSection from "../CardSection/CardSection";
 import LeftSideMenu from "../LeftSideMenu/LeftSideMenu";
 import "./Dashboard.css";
 function Dashboard() {
     return (
         <div className="dashboard">
             <LeftSideMenu />
+            <CardSection />
         </div>
     );
 }
