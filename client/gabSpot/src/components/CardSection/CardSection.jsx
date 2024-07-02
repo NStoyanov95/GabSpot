1b13554f54e58a5b0c299db82422dab40f1e856c
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-02 07:28:58 +0300

    Add dummy card data

--- client/gabSpot/src/components/CardSection/CardSection.jsx
+++ client/gabSpot/src/components/CardSection/CardSection.jsx
@@ -1,8 +1,26 @@
+import Card from "../Card/Card";
 import "./CardSection.css";
 function CardSection() {
+    const cards = [
+        {
+            authorImg:
+                "https://img.freepik.com/free-photo/curly-man-with-broad-smile-shows-perfect-teeth-being-amused-by-interesting-talk-has-bushy-curly-dark-hair-stands-indoor-against-white-blank-wall_273609-17092.jpg",
+            authorName: "John Doe",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores obcaecati corrupti amet id minus. Repellendus, porro dignissimos!",
+            contentImg:
+                "https://img.freepik.com/free-photo/dubai-city-landscape_1409-6484.jpg",
+            likes: 56,
+            comments: 4,
+        },
+    ];
     return (
         <div className="card-section">
-            <div className="card-container"></div>
+            <div className="card-container">
+                {cards.map((card) => (
+                    <Card card={card} />
+                ))}
+            </div>
         </div>
     );
 }
