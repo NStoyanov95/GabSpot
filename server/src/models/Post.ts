f2c866423b0aa27ad337ef77a60eac8ee2e0c1b5
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-07 00:44:20 +0300

    Edit post model

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -1,44 +1,49 @@
 import mongoose from "mongoose";
 
 interface PostDocument extends Document {
-  author: string;
-  content: string;
-  image: string;
-  comments: string[];
-  likes: string;
-  createdAt: Date;
-  updatedAt?: Date;
+    author: string;
+    authorImage: string;
+    content: string;
+    image: string;
+    comments: string[];
+    likes: string;
+    createdAt: Date;
+    updatedAt?: Date;
 }
 
 const postSchema = new mongoose.Schema(
-  {
-    author: {
-      type: String,
-      ref: "User",
+    {
+        author: {
+            type: String,
+            ref: "User",
+        },
+        authorImage: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        comments: [
+            {
+                type: String,
+            },
+        ],
+        likes: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "User",
+            },
+        ],
     },
-    content: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    comments: [
-      {
-        type: String,
-      },
-    ],
-    likes: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const Post = mongoose.model<PostDocument>("Post", postSchema);
