94933c21480ae26dd42f95c2d260a469ddd38bf5
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-06 22:22:05 +0300

    Implement fetching and rendering posts in Card component

--- client/gabSpot/src/components/Card/Card.jsx
+++ client/gabSpot/src/components/Card/Card.jsx
@@ -1,26 +1,26 @@
 import "./Card.css";
-function Card({ card }) {
+function Card({ post }) {
     return (
         <div className="card">
             <header className="card-header">
                 <div className="author-img">
-                    <img src={card.authorImg} />
+                    <img src={post.authorImg} />
                 </div>
                 <div className="author-name">
-                    <p>{card.authorName}</p>
+                    <p>{post.author}</p>
                 </div>
             </header>
             <div className="description">
-                <p>{card.description}</p>
+                <p>{post.content}</p>
             </div>
             <div className="content-image">
                 <a href="">
-                    <img src={card.contentImg} />
+                    <img src={post.image} />
                 </a>
             </div>
             <div className="likes-comments-container">
-                <p>Likes: {card.likes}</p>
-                <p>Comments: {card.comments}</p>
+                <p>Likes: {post.likes.length}</p>
+                <p>Comments: {post.comments.length}</p>
             </div>
             <footer className="card-footer">
                 <button>
--- client/gabSpot/src/components/CardSection/CardSection.jsx
+++ client/gabSpot/src/components/CardSection/CardSection.jsx
@@ -1,24 +1,23 @@
 import Card from "../Card/Card";
 import "./CardSection.css";
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../../services/postService";
 function CardSection() {
-    const cards = [
-        {
-            authorImg:
-                "https://img.freepik.com/free-photo/curly-man-with-broad-smile-shows-perfect-teeth-being-amused-by-interesting-talk-has-bushy-curly-dark-hair-stands-indoor-against-white-blank-wall_273609-17092.jpg",
-            authorName: "John Doe",
-            description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores obcaecati corrupti amet id minus. Repellendus, porro dignissimos!",
-            contentImg:
-                "https://img.freepik.com/free-photo/dubai-city-landscape_1409-6484.jpg",
-            likes: 56,
-            comments: 4,
-        },
-    ];
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const posts = await getAllPosts();
+            setPosts(posts);
+            console.log(posts);
+        })();
+    }, []);
+
     return (
         <div className="card-section">
             <div className="card-container">
-                {cards.map((card) => (
-                    <Card card={card} />
+                {posts.map((post) => (
+                    <Card key={post._id} post={post} />
                 ))}
             </div>
         </div>
