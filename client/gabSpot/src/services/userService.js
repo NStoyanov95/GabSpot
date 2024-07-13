972cd6524aa774c239e4f7365d3220274250da8d
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-13 11:54:51 +0300

    Create register service

---
+++ client/gabSpot/src/services/userService.js
@@ -0,0 +1,11 @@
+export const register = async (formData) => {
+    const res = await fetch("http://localhost:3030/users/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    return data;
+};
