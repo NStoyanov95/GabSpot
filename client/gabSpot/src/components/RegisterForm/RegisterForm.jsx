84162c0f3fcba4f7cb9ab309b3c3351c1fa9d69e
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-13 11:45:40 +0300

    Add controlled form functionality to register form

--- client/gabSpot/src/components/RegisterForm/RegisterForm.jsx
+++ client/gabSpot/src/components/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
 import "./RegisterForm.css";
 function RegisterForm() {
+    const [formData, setFormData] = useState({
+        username: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        profileImage: "",
+        password: "",
+        repeatPassword: "",
+    });
+
+    const changeHandler = (e) => {
+        setFormData((oldState) => ({
+            ...oldState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log(formData);
+        setFormData({
+            username: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            profileImage: "",
+            password: "",
+            repeatPassword: "",
+        });
+    };
     return (
         <>
             <div className="form-container">
@@ -14,6 +46,8 @@ function RegisterForm() {
                                 name="username"
                                 id="username"
                                 placeholder="Username"
+                                onChange={changeHandler}
+                                value={formData.username}
                             />
                             <label htmlFor="username">Username</label>
                         </div>
@@ -23,6 +57,8 @@ function RegisterForm() {
                                 name="firstName"
                                 id="first-name"
                                 placeholder="First Name"
+                                onChange={changeHandler}
+                                value={formData.firstName}
                             />
                             <label htmlFor="firstName">First Name</label>
                         </div>
@@ -32,6 +68,8 @@ function RegisterForm() {
                                 name="lastName"
                                 id="last-name"
                                 placeholder="Last Name"
+                                onChange={changeHandler}
+                                value={formData.lastName}
                             />
                             <label htmlFor="lastName">Last Name</label>
                         </div>
@@ -41,6 +79,8 @@ function RegisterForm() {
                                 name="email"
                                 id="email"
                                 placeholder="Email"
+                                onChange={changeHandler}
+                                value={formData.email}
                             />
                             <label htmlFor="email">Email</label>
                         </div>
@@ -50,6 +90,8 @@ function RegisterForm() {
                                 name="profileImage"
                                 id="profile-image"
                                 placeholder="profile image"
+                                onChange={changeHandler}
+                                value={formData.profileImage}
                             />
                             <label htmlFor="profile-image">
                                 Profile image URL
@@ -61,6 +103,8 @@ function RegisterForm() {
                                 name="password"
                                 id="password"
                                 placeholder="Password"
+                                onChange={changeHandler}
+                                value={formData.password}
                             />
                             <label htmlFor="password">Password</label>
                         </div>
@@ -70,6 +114,8 @@ function RegisterForm() {
                                 name="repeatPassword"
                                 id="repeat-password"
                                 placeholder="Confirm Password"
+                                onChange={changeHandler}
+                                value={formData.repeatPassword}
                             />
                             <label htmlFor="repeatPassword">
                                 Confirm Password
@@ -79,6 +125,7 @@ function RegisterForm() {
                             type="submit"
                             value="Sign Up"
                             className="submit-btn"
+                            onClick={submitHandler}
                         />
                         <div className="loginRedirect">
                             <p>
