ba2beec86433f9620e399efa93adaa51e672a112
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-13 21:18:13 +0300

    Create login functionality

--- client/gabSpot/src/services/userService.js
+++ client/gabSpot/src/services/userService.js
@@ -9,3 +9,15 @@ export const register = async (formData) => {
     const data = await res.json();
     return data;
 };
+
+export const login = async (formData) => {
+    const res = await fetch("http://localhost:3030/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    return data;
+};
