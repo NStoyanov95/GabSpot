7c67611c08e5aedd434d38f645d0867da94bdf1b
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-13 11:37:41 +0300

    Show static register form

--- client/gabSpot/src/App.jsx
+++ client/gabSpot/src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header/Header";
 import Homepage from "./components/Homepage/Homepage";
 import Dashboard from "./components/Dashboard/Dashboard";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
     return (
         <>
             <Header />
             {/* <Homepage /> */}
-            <Dashboard />
+            {/* <Dashboard /> */}
+            <RegisterForm />
         </>
     );
 }
