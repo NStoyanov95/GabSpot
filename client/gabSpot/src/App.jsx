00ef646177f6f5b84a9254136a9f1d112b3bd9b1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-14 19:41:08 +0300

    Add routes

--- client/gabSpot/src/App.jsx
+++ client/gabSpot/src/App.jsx
@@ -4,16 +4,18 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import LoginForm from "./components/LoginForm/LoginForm";
 import CreatePostForm from "./components/CreatePostForm/CreatePostForm";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
     return (
         <>
             <Header />
-            {/* <Homepage /> */}
-            {/* <Dashboard /> */}
-            {/* <RegisterForm /> */}
-            {/* <LoginForm /> */}
-            <CreatePostForm />
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/createpost" element={<CreatePostForm />} />
+            </Routes>
         </>
     );
 }
