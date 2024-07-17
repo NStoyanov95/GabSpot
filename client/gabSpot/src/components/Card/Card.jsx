dc9b391616e4ab4ffae35c0b11339e7a53de805c
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-18 00:25:19 +0300

    Add link to details page

--- client/gabSpot/src/components/Card/Card.jsx
+++ client/gabSpot/src/components/Card/Card.jsx
@@ -1,34 +1,37 @@
+import { Link } from "react-router-dom";
 import "./Card.css";
 function Card({ post }) {
     return (
-        <div className="card">
-            <header className="card-header">
-                <div className="author-img">
-                    <img src={post.authorImage} />
+        <Link to={`/details/${post._id}`}>
+            <div className="card">
+                <header className="card-header">
+                    <div className="author-img">
+                        <img src={post.authorImage} />
+                    </div>
+                    <div className="author-name">
+                        <p>{post.author}</p>
+                    </div>
+                </header>
+                <div className="description">
+                    <p>{post.content}</p>
                 </div>
-                <div className="author-name">
-                    <p>{post.author}</p>
+                <div className="content-image">
+                    <img src={post.image} />
                 </div>
-            </header>
-            <div className="description">
-                <p>{post.content}</p>
+                <div className="likes-comments-container">
+                    <p>Likes: {post.likes.length}</p>
+                    <p>Comments: {post.comments.length}</p>
+                </div>
+                <footer className="card-footer">
+                    <button>
+                        <i className="fas fa-heart"></i>
+                    </button>
+                    <button>
+                        <i className="fas fa-comment"></i>
+                    </button>
+                </footer>
             </div>
-            <div className="content-image">
-                <img src={post.image} />
-            </div>
-            <div className="likes-comments-container">
-                <p>Likes: {post.likes.length}</p>
-                <p>Comments: {post.comments.length}</p>
-            </div>
-            <footer className="card-footer">
-                <button>
-                    <i className="fas fa-heart"></i>
-                </button>
-                <button>
-                    <i className="fas fa-comment"></i>
-                </button>
-            </footer>
-        </div>
+        </Link>
     );
 }
 export default Card;
