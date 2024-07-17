5677e28bf0102ebb37c1a7d88185e73351f21658
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-18 00:35:17 +0300

    Update css

--- client/gabSpot/src/components/Card/Card.jsx
+++ client/gabSpot/src/components/Card/Card.jsx
@@ -2,36 +2,36 @@ import { Link } from "react-router-dom";
 import "./Card.css";
 function Card({ post }) {
     return (
-        <Link to={`/details/${post._id}`}>
-            <div className="card">
-                <header className="card-header">
-                    <div className="author-img">
-                        <img src={post.authorImage} />
-                    </div>
-                    <div className="author-name">
-                        <p>{post.author}</p>
-                    </div>
-                </header>
-                <div className="description">
-                    <p>{post.content}</p>
+        <div className="card">
+            <header className="card-header">
+                <div className="author-img">
+                    <img src={post.authorImage} />
                 </div>
-                <div className="content-image">
-                    <img src={post.image} />
+                <div className="author-name">
+                    <p>{post.author}</p>
                 </div>
-                <div className="likes-comments-container">
-                    <p>Likes: {post.likes.length}</p>
-                    <p>Comments: {post.comments.length}</p>
-                </div>
-                <footer className="card-footer">
-                    <button>
-                        <i className="fas fa-heart"></i>
-                    </button>
-                    <button>
-                        <i className="fas fa-comment"></i>
-                    </button>
-                </footer>
+            </header>
+            <div className="description">
+                <p>{post.content}</p>
             </div>
-        </Link>
+            <div className="content-image">
+                <Link to={`/details/${post._id}`}>
+                    <img src={post.image} />
+                </Link>
+            </div>
+            <div className="likes-comments-container">
+                <p>Likes: {post.likes.length}</p>
+                <p>Comments: {post.comments.length}</p>
+            </div>
+            <footer className="card-footer">
+                <button>
+                    <i className="fas fa-heart"></i>
+                </button>
+                <button>
+                    <i className="fas fa-comment"></i>
+                </button>
+            </footer>
+        </div>
     );
 }
 export default Card;
