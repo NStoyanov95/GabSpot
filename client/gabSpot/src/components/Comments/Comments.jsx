23fc5cf29dee0033d1b13f2462f37677806cf1e9
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-18 18:21:30 +0300

    Create comments component

---
+++ client/gabSpot/src/components/Comments/Comments.jsx
@@ -0,0 +1,15 @@
+import styles from "./Comments.module.css";
+function Comments({ comment }) {
+    return (
+        <div className={styles.comment}>
+            <div className={styles["comment-content"]}>
+                <p className={styles["comment-text"]}>{comment}</p>
+                <button className={styles["delete-btn"]}>
+                    <i className="fas fa-trash" /> Delete
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Comments;
