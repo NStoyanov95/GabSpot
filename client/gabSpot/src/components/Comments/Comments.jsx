850ae5ed1cd9cffc4b520c263f43f588dc041865
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-19 17:51:04 +0300

    Update css

--- client/gabSpot/src/components/Comments/Comments.jsx
+++ client/gabSpot/src/components/Comments/Comments.jsx
@@ -5,7 +5,7 @@ function Comments({ comment }) {
             <div className={styles["comment-content"]}>
                 <p className={styles["comment-text"]}>{comment}</p>
                 <button className={styles["delete-btn"]}>
-                    <i className="fas fa-trash" /> Delete
+                    <i className="fas fa-trash" />
                 </button>
             </div>
         </div>
--- client/gabSpot/src/components/DetailsCard/DetailsCard.jsx
+++ client/gabSpot/src/components/DetailsCard/DetailsCard.jsx
@@ -36,51 +36,57 @@ function DetailsCard() {
     return (
         <div className={styles["post-container"]}>
             <div className={styles["post-card"]}>
-                <div className={styles["post-header"]}>
-                    <div className={styles["post-header-info"]}>
-                        <img
-                            src={post.authorImage}
-                            alt="User Avatar"
-                            className={styles.avatar}
-                        />
-                        <div className={styles["user-info"]}>
-                            <h4 className={styles.username}>{post.author}</h4>
-                            <p className={styles["post-date"]}>
-                                Posted on {post.createdAt}
-                            </p>
+                <div className={styles["left-section"]}>
+                    <div className={styles["post-header"]}>
+                        <div className={styles["post-header-info"]}>
+                            <img
+                                src={post.authorImage}
+                                alt="User Avatar"
+                                className={styles.avatar}
+                            />
+                            <div className={styles["user-info"]}>
+                                <h4 className={styles.username}>
+                                    {post.author}
+                                </h4>
+                                <p className={styles["post-date"]}>
+                                    Posted on {post.createdAt}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles["post-content"]}>
+                        <p>{post.content}</p>
+                        <img src={post.image} alt="Post Image" />
+                        <div className={styles["post-buttons"]}>
+                            <button className={styles["like-btn"]}>
+                                <i className="fas fa-thumbs-up" /> Like
+                            </button>
+                            <button className={styles["edit-btn"]}>
+                                <i className="fas fa-edit" /> Edit
+                            </button>
+                            <button className={styles["delete-btn"]}>
+                                <i className="fas fa-trash" /> Delete
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div className={styles["post-content"]}>
-                    <p>{post.content}</p>
-                    <img src={post.image} alt="Post Image" />
-                    <div className={styles["post-buttons"]}>
-                        <button className={styles["like-btn"]}>
-                            <i className="fas fa-thumbs-up" /> Like
-                        </button>
-                        <button className={styles["edit-btn"]}>
-                            <i className="fas fa-edit" /> Edit
-                        </button>
-                        <button className={styles["delete-btn"]}>
-                            <i className="fas fa-trash" /> Delete
-                        </button>
+                <div className={styles["right-section"]}>
+                    <div className={styles["comments-section"]}>
+                        <h4>Comments</h4>
+                        {comments.map((comment) => (
+                            <Comments key={comment} comment={comment} />
+                        ))}
+                        <form className={styles["comment-form"]}>
+                            <textarea
+                                placeholder="Add a comment..."
+                                onChange={changeHandler}
+                                value={newComment}
+                            />
+                            <button type="submit" onClick={onSubmit}>
+                                Post Comment
+                            </button>
+                        </form>
                     </div>
-                </div>
-                <div className={styles["comments-section"]}>
-                    <h4>Comments</h4>
-                    {comments.map((comment) => (
-                        <Comments key={comment} comment={comment} />
-                    ))}
-                    <form className={styles["comment-form"]}>
-                        <textarea
-                            placeholder="Add a comment..."
-                            onChange={changeHandler}
-                            value={newComment}
-                        />
-                        <button type="submit" onClick={onSubmit}>
-                            Post Comment
-                        </button>
-                    </form>
                 </div>
             </div>
         </div>
