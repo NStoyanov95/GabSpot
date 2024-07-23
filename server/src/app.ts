d81ecbe2dcc656cc70da9fbe05661275e2747bbd
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-23 23:44:57 +0300

    Add cors options

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,23 +7,27 @@ import dotenv from "dotenv";
 import router from "./routes";
 
 dotenv.config();
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+};
 
 mongoose
-  .connect(process.env.MONGO_URI!)
-  .then(() => {
-    console.log("Db connected");
-  })
-  .catch((err) => console.error("Database connection error", err));
+    .connect(process.env.MONGO_URI!)
+    .then(() => {
+        console.log("Db connected");
+    })
+    .catch((err) => console.error("Database connection error", err));
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(router);
 
 const port: string = process.env.PORT || "3030";
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
