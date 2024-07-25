62caad63a84065c6abd4375e574499c8587c9daf
NStoyanov95 <nstoyanov9512@gmail.com>
2024-07-25 21:11:04 +0300

    Throw result if server response is not ok

--- client/gabSpot/src/services/api.js
+++ client/gabSpot/src/services/api.js
@@ -11,6 +11,9 @@ const requester = async (url, method = "GET", data = null) => {
     }
     const res = await fetch(baseUrl + url, options);
     const result = await res.json();
+    if (!res.ok) {
+        throw result;
+    }
     return result;
 };
 export default requester;
