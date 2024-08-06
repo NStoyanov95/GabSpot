b26fb00c2aad10bf3bca591c0b7539bff831b4cd
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-06 08:05:52 +0300

    Add loader

--- client/gabSpot/src/components/CardSection/CardSection.jsx
+++ client/gabSpot/src/components/CardSection/CardSection.jsx
@@ -2,22 +2,33 @@ import Card from "../Card/Card";
 import "./CardSection.css";
 import { useEffect, useState } from "react";
 import { getAllPosts } from "../../services/postService";
+import Loader from "../Loader/Loader";
 function CardSection() {
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         (async () => {
-            const posts = await getAllPosts();
-            setPosts(posts);
+            try {
+                setIsLoading(true);
+                const posts = await getAllPosts();
+                setPosts(posts);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                console.log(error);
+            }
         })();
     }, []);
 
     return (
         <div className="card-section">
             <div className="card-container">
-                {posts.map((post) => (
-                    <Card key={post._id} post={post} />
-                ))}
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    posts.map((post) => <Card key={post._id} post={post} />)
+                )}
             </div>
         </div>
     );
--- client/gabSpot/src/components/ProfileCard/ProfileCard.jsx
+++ client/gabSpot/src/components/ProfileCard/ProfileCard.jsx
@@ -2,84 +2,98 @@ import styles from "./ProfileCard.module.css";
 import { useEffect, useState } from "react";
 import { getUserWithPosts } from "../../services/userService";
 import { Link, useParams } from "react-router-dom";
+import Loader from "../Loader/Loader";
 
 function ProfileCard() {
     const { userId } = useParams();
     const [user, setUser] = useState({});
     const [createdPost, setCreatedPost] = useState([]);
     const [likedPost, setLikedPost] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         (async () => {
-            const data = await getUserWithPosts(userId);
-            setUser(data.user);
-            setCreatedPost(data.createdPosts);
-            setLikedPost(data.likedPosts);
-            console.log(data);
+            try {
+                setIsLoading(true);
+                const data = await getUserWithPosts(userId);
+                setUser(data.user);
+                setCreatedPost(data.createdPosts);
+                setLikedPost(data.likedPosts);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                console.log(error);
+            }
         })();
     }, []);
 
     return (
         <div className={styles.profileContainer}>
-            <div className={styles.profile}>
-                <img
-                    src={user.profileImage}
-                    alt="Profile Avatar"
-                    className={styles.avatar}
-                />
-                <h2 className={styles.name}>
-                    {user.firstName} {user.lastName}
-                </h2>
-                <p className={styles.email}>{user.email}</p>
-            </div>
-            <div className={styles.postsContainer}>
-                <div className={styles.section}>
-                    <h3>My Posts</h3>
-                    {createdPost.length !== 0 ? (
-                        <div className={styles.postsGrid}>
-                            {createdPost.map((post) => (
-                                <Link
-                                    to={`/details/${post._id}`}
-                                    key={post._id}
-                                >
-                                    <img
-                                        src={post.image}
-                                        alt="My Post"
-                                        className={styles.postImage}
-                                    />
-                                </Link>
-                            ))}
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <div className={styles.profile}>
+                        <img
+                            src={user.profileImage}
+                            alt="Profile Avatar"
+                            className={styles.avatar}
+                        />
+                        <h2 className={styles.name}>
+                            {user.firstName} {user.lastName}
+                        </h2>
+                        <p className={styles.email}>{user.email}</p>
+                    </div>
+                    <div className={styles.postsContainer}>
+                        <div className={styles.section}>
+                            <h3>My Posts</h3>
+                            {createdPost.length !== 0 ? (
+                                <div className={styles.postsGrid}>
+                                    {createdPost.map((post) => (
+                                        <Link
+                                            to={`/details/${post._id}`}
+                                            key={post._id}
+                                        >
+                                            <img
+                                                src={post.image}
+                                                alt="My Post"
+                                                className={styles.postImage}
+                                            />
+                                        </Link>
+                                    ))}
+                                </div>
+                            ) : (
+                                <div>
+                                    <p>You haven't created any posts yet.</p>
+                                </div>
+                            )}
                         </div>
-                    ) : (
-                        <div>
-                            <p>You haven't created any posts yet.</p>
+                        <div className={styles.section}>
+                            <h3>Liked Posts</h3>
+                            {likedPost.length !== 0 ? (
+                                <div className={styles.postsGrid}>
+                                    {likedPost.map((post) => (
+                                        <Link
+                                            to={`/details/${post._id}`}
+                                            key={post._id}
+                                        >
+                                            <img
+                                                src={post.image}
+                                                alt="Liked Post"
+                                                className={styles.postImage}
+                                            />
+                                        </Link>
+                                    ))}
+                                </div>
+                            ) : (
+                                <div>
+                                    <p>You haven't liked any posts yet.</p>
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
-                <div className={styles.section}>
-                    <h3>Liked Posts</h3>
-                    {likedPost.length !== 0 ? (
-                        <div className={styles.postsGrid}>
-                            {likedPost.map((post) => (
-                                <Link
-                                    to={`/details/${post._id}`}
-                                    key={post._id}
-                                >
-                                    <img
-                                        src={post.image}
-                                        alt="Liked Post"
-                                        className={styles.postImage}
-                                    />
-                                </Link>
-                            ))}
-                        </div>
-                    ) : (
-                        <div>
-                            <p>You haven't liked any posts yet.</p>
-                        </div>
-                    )}
-                </div>
-            </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
