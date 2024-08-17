88bb9e155fa3edbabf500bfc7696ff732259254f
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-17 17:06:02 +0300

    Add registered users to right side menu component

--- client/gabSpot/src/components/RightSideMenu/RightSideMenu.jsx
+++ client/gabSpot/src/components/RightSideMenu/RightSideMenu.jsx
@@ -1,11 +1,13 @@
 import style from "./RightSideMenu.module.css";
 import MostLikedPost from "../MostLikedPost/MostLikedPost";
+import RegisteredUsers from "../RegisteredUsers/RegisteredUsers";
 
 function RightSideMenu(params) {
     return (
         <>
             <div className={style["right-side-menu"]}>
                 <MostLikedPost />
+                <RegisteredUsers />
             </div>
         </>
     );
