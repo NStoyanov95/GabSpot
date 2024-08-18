49cfa057f2323cc9fa668f5ba1fafec5fe900fba
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-18 08:08:53 +0300

    Create register users list item component

---
+++ client/gabSpot/src/components/RegisteredUsers/RegisteredUsersList/RegisteredUserListItem.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import style from "./RegisteredUserListItem.module.css";
+
+function RegisteredUsersListItem({ user }) {
+    return (
+        <>
+            <div className={style["user-list"]} key={user._id}>
+                <img src={user.profileImage} alt="user" />
+                <p>
+                    {user.firstName} {user.lastName}
+                </p>
+                <div className={style["buttons"]}>
+                    <Link>
+                        <i className="fas fa-user"></i>
+                    </Link>
+                    <Link>
+                        <i className="fas fa-comment"></i>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default RegisteredUsersListItem;
