0728ff6b4b9f4e330704b26649c500c77a8c9e67
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-24 08:02:41 +0300

    Show message modal when click on send message button

--- client/gabSpot/src/components/RegisteredUsers/RegisteredUsersList/RegisteredUserListItem.jsx
+++ client/gabSpot/src/components/RegisteredUsers/RegisteredUsersList/RegisteredUserListItem.jsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 import style from "./RegisteredUserListItem.module.css";
+import SendMessageModal from "../../SendMessageModal/SendMessageModal";
+import { useState } from "react";
 
 function RegisteredUsersListItem({ user }) {
+    const [isMessageModalShown, setIsMessageModalShown] = useState(false);
+
+    const closeMessageModal = () => {
+        setIsMessageModalShown(false);
+    };
+
     return (
         <>
+            {isMessageModalShown && (
+                <SendMessageModal
+                    closeMessageModal={closeMessageModal}
+                    user={user}
+                />
+            )}
             <div className={style["user-list"]} key={user._id}>
                 <img src={user.profileImage} alt="user" />
                 <p>
@@ -13,7 +27,7 @@ function RegisteredUsersListItem({ user }) {
                     <Link to={`/profile/${user._id}`}>
                         <i className="fas fa-user"></i>
                     </Link>
-                    <Link>
+                    <Link onClick={() => setIsMessageModalShown(true)}>
                         <i className="fas fa-comment"></i>
                     </Link>
                 </div>
