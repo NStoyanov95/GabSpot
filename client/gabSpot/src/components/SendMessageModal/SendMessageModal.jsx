fdc4ab234fbc44f8a679e31cd80cb058e33fecb3
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-24 08:03:07 +0300

    Implement controlled form to the message modal

--- client/gabSpot/src/components/SendMessageModal/SendMessageModal.jsx
+++ client/gabSpot/src/components/SendMessageModal/SendMessageModal.jsx
@@ -1,21 +1,56 @@
+import { useState } from "react";
 import styles from "./SendMessageModal.module.css";
 
-function SendMessageModal({ closeMessageModal }) {
+function SendMessageModal({ closeMessageModal, user }) {
+    const [formValue, setFormValue] = useState({
+        to: user.username,
+        message: "",
+    });
+
+    const changeHandler = (e) => {
+        setFormValue((oldState) => ({
+            ...oldState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const stopPropagation = (e) => {
+        e.stopPropagation();
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(formValue);
+    };
     return (
         <>
             <div className={styles.modalOverlay} onClick={closeMessageModal}>
-                <div className={styles.modalContent}>
+                <div className={styles.modalContent} onClick={stopPropagation}>
                     <h4>Send message</h4>
                     <form className={styles.form}>
                         <div className={styles.field}>
                             <label htmlFor="to">To</label>
-                            <input type="text" id="to" readOnly value="Stoqn" />
+                            <input
+                                type="text"
+                                name="to"
+                                id="to"
+                                readOnly
+                                value={formValue.to}
+                            />
                         </div>
                         <div className={styles.field}>
                             <label htmlFor="message">Message</label>
-                            <textarea type="text" id="message" />
+                            <textarea
+                                type="text"
+                                id="message"
+                                name="message"
+                                value={formValue.message}
+                                onChange={changeHandler}
+                            />
                         </div>
-                        <button className={styles.submitBtn}>Send</button>
+                        <button className={styles.submitBtn} onClick={onSubmit}>
+                            Send
+                        </button>
                     </form>
                 </div>
             </div>
