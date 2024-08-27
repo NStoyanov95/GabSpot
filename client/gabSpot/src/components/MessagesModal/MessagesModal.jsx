3341e5bd8d6c45289d570a6360a9f4fc0101184e
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-27 22:28:31 +0300

    Implement message fetching and rendering with dynamic data

--- client/gabSpot/src/components/MessagesModal/MessagesModal.jsx
+++ client/gabSpot/src/components/MessagesModal/MessagesModal.jsx
@@ -1,22 +1,32 @@
+import { useContext, useEffect, useState } from "react";
 import styles from "./MessagesModal.module.css";
+import AuthContext from "../../contexts/AuthContext";
+import { getMessagesForUser } from "../../services/messageService";
+import MessageListItem from "./MessageListItem/MessageListItem";
 function MessagesModal() {
+    const { userId } = useContext(AuthContext);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getMessagesForUser(userId);
+            setMessages(data);
+            console.log(data);
+        })();
+    }, []);
     return (
         <>
             <div className={styles.modalOverlay}>
                 <div className={styles.modalContent}>
                     <h2>Messsages</h2>
-                    <div className={styles.messageContainer}>
-                        <h5>Gosho:</h5>
-                        <p>Hello, My name is Stoyan, nice to meet you!</p>
-                    </div>
-                    <div className={styles.messageContainer}>
-                        <h5>Gosho:</h5>
-                        <p>Hello, My name is Stoyan, nice to meet you!</p>
-                    </div>
-                    <div className={styles.messageContainer}>
-                        <h5>Gosho:</h5>
-                        <p>Hello, My name is Stoyan, nice to meet you!</p>
-                    </div>
+                    {messages.length < 0
+                        ? "No messages yet!"
+                        : messages.map((message) => (
+                              <MessageListItem
+                                  message={message}
+                                  key={message._id}
+                              />
+                          ))}
                 </div>
             </div>
         </>
