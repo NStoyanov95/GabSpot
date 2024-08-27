e5e922a9e81de3a7766e49ff152817ec3a72e427
NStoyanov95 <nstoyanov9512@gmail.com>
2024-08-27 21:59:33 +0300

    Create get all messages for single user service

--- client/gabSpot/src/services/messageService.js
+++ client/gabSpot/src/services/messageService.js
@@ -4,3 +4,8 @@ export const sendMessage = async (formData) => {
     const data = await requester("messages/send", "POST", formData);
     return data;
 };
+
+export const getMessagesForUser = async (userId) => {
+    const data = await requester(`messages/${userId}`, "GET");
+    return data;
+};
