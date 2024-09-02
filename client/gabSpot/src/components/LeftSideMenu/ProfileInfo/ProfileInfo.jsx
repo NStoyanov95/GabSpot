eaeee9a0aba0fad96e2a7859fb74c83215b27659
NStoyanov95 <nstoyanov9512@gmail.com>
2024-09-02 21:56:48 +0300

    Navigate to profile when click on the profile photo

--- client/gabSpot/src/components/LeftSideMenu/ProfileInfo/ProfileInfo.jsx
+++ client/gabSpot/src/components/LeftSideMenu/ProfileInfo/ProfileInfo.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import styles from "./ProfileInfo.module.css";
 
 function ProfileInfo({ currentUser }) {
     return (
         <>
             <div className={styles.profileInfo}>
-                <a href="#">
+                <Link to={`/profile/${currentUser._id}`}>
                     <div className={styles.profileImg}>
                         <img src={currentUser.profileImage} alt="Profile" />
                     </div>
                     <div className={styles.profileName}>
                         <p>{currentUser.username}</p>
                     </div>
-                </a>
+                </Link>
             </div>
         </>
     );
